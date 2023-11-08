74fd0fd801177a7f9aa7a38321407cefd3480a6e
Jason Liang <jasonliang512@gmail.com>
2023-11-07 18:05:18 -0800

    pathfinding example

---
+++ examples/pathfinding/main.lua
@@ -0,0 +1,24 @@
+function spry.conf(t)
+  t.swap_interval = 1
+  t.target_fps = 0
+end
+
+function spry.start()
+  font = spry.default_font()
+  map = spry.tilemap_load "map.ldtk"
+  map:make_graph('IntGrid', { [1] = 1 })
+  map:print_graph()
+end
+
+function spry.frame(dt)
+  if spry.platform() ~= "html5" and spry.key_down "esc" then
+    spry.quit()
+  end
+
+  spry.push_matrix()
+  spry.scale(3, 3)
+    map:draw()
+  spry.pop_matrix()
+
+  font:draw(("fps: %.2f (%.4f)"):format(1 / dt, dt * 1000))
+end
